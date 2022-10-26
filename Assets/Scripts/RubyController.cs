75be46a14f87f84eb46da8aa2692c711eb03e13c
JesusJMUJI <al427616@uji.es>
2022-10-26 19:01:24 +0200

    Commit de prueba desde Rider

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -2,16 +2,11 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    private void Start()
-    {
-
-    }
-
-    void Update() 
+    void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        
+
         Vector2 position = transform.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
