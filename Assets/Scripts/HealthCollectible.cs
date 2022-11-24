7070f598ec547302b7b7950e221560fe1db7e1a2
Jesus_JM_UJI <al427616@uji.es>
2022-11-24 16:10:29 +0100

    Cosillas

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -2,13 +2,25 @@
 
 public class HealthCollectible : MonoBehaviour
 {
+    public ParticleSystem collectEffect;
+    void Start()
+    {
+        GameObject go = Instantiate(collectEffect.gameObject);
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         RubyController controller = other.GetComponent<RubyController>();
         if (controller.currentHealth < controller.maxHealth)
         {
-            controller.ChangeHealth(1);
-            Destroy(gameObject);
+            Despawn(controller);
         }
+    }
+
+    private void Despawn(RubyController controller)
+    {
+        collectEffect.Play();
+        gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        controller.ChangeHealth(1);
+        Destroy(gameObject, 2);
     }
 }
